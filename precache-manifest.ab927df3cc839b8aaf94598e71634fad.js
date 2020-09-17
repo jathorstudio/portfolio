a8418fd6a77f9eeb9520c6bb7a48d787e5fd896c
Daniel Gazcon <daniel.gazcon27@gmail.com>
2020-09-17 17:04:24 -0400

    Updates

---
+++ precache-manifest.ab927df3cc839b8aaf94598e71634fad.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c4b3898b73c511b4576576d5c0f88d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2e8be6c5c7601801c4c3",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6890d64356bc9ecb5a67",
+    "url": "/portfolio/static/css/main.b3fceccf.chunk.css"
+  },
+  {
+    "revision": "2e8be6c5c7601801c4c3",
+    "url": "/portfolio/static/js/2.d364ea38.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.d364ea38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6890d64356bc9ecb5a67",
+    "url": "/portfolio/static/js/main.01e4b13b.chunk.js"
+  },
+  {
+    "revision": "db38150483e251c9320e",
+    "url": "/portfolio/static/js/runtime-main.e5991c6d.js"
+  },
+  {
+    "revision": "885e5558c90864ddb72ad486f36470cc",
+    "url": "/portfolio/static/media/RobotoCondensed-Light.885e5558.ttf"
+  },
+  {
+    "revision": "0b01fc09af1a67160397ebbd676c7b5a",
+    "url": "/portfolio/static/media/banner.0b01fc09.jpg"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/portfolio/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/portfolio/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/portfolio/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/portfolio/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/portfolio/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "73129a4352952fdc00fcc7e55ef7a52f",
+    "url": "/portfolio/static/media/img_parallax3.73129a43.jpg"
+  },
+  {
+    "revision": "92305e7223b801b427b8a6a3b1c3ac44",
+    "url": "/portfolio/static/media/pic01.92305e72.jpg"
+  },
+  {
+    "revision": "7a1cc4cec8edb4bfc754642868db0693",
+    "url": "/portfolio/static/media/pic02.7a1cc4ce.jpg"
+  },
+  {
+    "revision": "28b540b353c1c2915ec58a845ac3f2d9",
+    "url": "/portfolio/static/media/pic03.28b540b3.jpg"
+  },
+  {
+    "revision": "9aedd6f1b393a718bb1db19cdf3887aa",
+    "url": "/portfolio/static/media/pic04.9aedd6f1.jpg"
+  },
+  {
+    "revision": "ee1864421c844f4af8305a3112d8b73f",
+    "url": "/portfolio/static/media/pic05.ee186442.jpg"
+  },
+  {
+    "revision": "542ba962741138d27b795723c2f6f627",
+    "url": "/portfolio/static/media/pic06.542ba962.jpg"
+  }
+]);
