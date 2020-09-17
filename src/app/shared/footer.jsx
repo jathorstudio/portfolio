0e3c375ba327a8b9a48ea32dd404e353320d87b1
Daniel Gazcon <daniel.gazcon27@gmail.com>
2020-09-17 16:53:43 -0400

    publish jathorstudio

--- src/app/shared/footer.jsx
+++ src/app/shared/footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
 		<footer className="container-division">
 			<div>
 				<ul className="icons">
-					<li><a target="_blank" href="https://www.linkedin.com/in/dgazcon/"><FontAwesomeIcon icon={['fab','linkedin-in']} /></a></li>
-					<li><a target="_blank" href="https://twitter.com/dgazcon"><FontAwesomeIcon icon={['fab','twitter']} /></a></li>
-					<li><a target="_blank" href="https://www.instagram.com/jathorstudio"><FontAwesomeIcon icon={['fab','instagram']} /></a></li>
+					<li><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/dgazcon/"><FontAwesomeIcon icon={['fab','linkedin-in']} /></a></li>
+					<li><a rel="noopener noreferrer" target="_blank" href="https://twitter.com/dgazcon"><FontAwesomeIcon icon={['fab','twitter']} /></a></li>
+					<li><a rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/jathorstudio"><FontAwesomeIcon icon={['fab','instagram']} /></a></li>
 				</ul>
 			</div>
 		</footer>
